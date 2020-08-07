0fe2cdfc836ec88601395e00f9a30a0ab8251c8f
paspartuuu <danworkmail@yandex.ru>
2020-08-07 13:17:15 +0300

    add groupsHandler

--- main/webServer.go
+++ main/webServer.go
@@ -1,5 +1,45 @@
 package main
 
-func main() {
+import (
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
 
+type group struct {
+	Name        string `json:"group_name"`
+	Description string `json:"group_description"`
+	GroupID     int    `json:"group_id"`
+	ParentID    int    `json:"parent_id"`
+}
+
+type task struct {
+}
+
+var groups = readGroups()
+
+func readGroups() []group {
+	groupsFile, err := ioutil.ReadFile("groups.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+	var groups []group
+	err = json.Unmarshal(groupsFile, &groups)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return groups
+}
+
+func groupsHandler(w http.ResponseWriter, r *http.Request) {
+	err := json.NewEncoder(w).Encode(groups)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func main() {
+	http.HandleFunc("/groups", groupsHandler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
