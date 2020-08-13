c95edb77bd0c1feb55049aa8fa75320a443bb4b4
paspartuuu <danworkmail@yandex.ru>
2020-08-13 14:13:39 +0300

    add tasksListHandler funcs

--- main/webServer.go
+++ main/webServer.go
@@ -7,6 +7,7 @@ import (
 	"log"
 	"net/http"
 	"strconv"
+	"time"
 )
 
 type group struct {
@@ -17,9 +18,17 @@ type group struct {
 }
 
 type task struct {
+	TaskID        string    `json:"task_id"`
+	GroupID       int       `json:"group_id"`
+	Task          string    `json:"task"`
+	Completed     bool      `json:"completed"`
+	CreatedDate   time.Time `json:"created_at"`
+	CompletedDate time.Time `json:"completed_at"`
 }
 
 var taskGroups = readGroups()
+
+var tasks = readTasks()
 
 func readGroups() []group {
 	groupsFile, err := ioutil.ReadFile("groups.json")
@@ -34,7 +43,25 @@ func readGroups() []group {
 	return groups
 }
 
-func groupsHandler(w http.ResponseWriter, r *http.Request) {
+func readTasks() []task {
+	tasksFile, err := ioutil.ReadFile("tasks.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+	var newTasks []task
+	err = json.Unmarshal(tasksFile, &newTasks)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return newTasks
+}
+
+func groupsListHandler(w http.ResponseWriter, r *http.Request) {
+	method := r.Method
+	if method != "GET" {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
 	l := r.URL.Query().Get("limit")
 	s := r.URL.Query().Get("sort")
 	newGroups := getSortedGroups(taskGroups, s, l)
@@ -48,11 +75,13 @@ func getSortedGroups(g []group, s string, l string) []group {
 	var newGroups []group
 	switch s {
 	case "name":
-		newGroups = sortByName(g, 0, len(g))
+		newGroups = sortGroupsByName(g, 0, len(g))
 	case "parents_first":
 		newGroups = sortByParentsFirst(g)
 	case "parent_with_children":
 		newGroups = sortByParentWithChildren(g)
+	default:
+		newGroups = g
 	}
 	lim, err := strconv.Atoi(l)
 	if err != nil || lim < 0 {
@@ -65,7 +94,7 @@ func getSortedGroups(g []group, s string, l string) []group {
 	return newGroups
 }
 
-func sortByName(grs []group, s int, e int) []group {
+func sortGroupsByName(grs []group, s int, e int) []group {
 	for i := s + 1; i < e; i++ {
 		if grs[i].Name < grs[i-1].Name {
 			gr := grs[i]
@@ -91,7 +120,7 @@ func sortByParentsFirst(grs []group) []group {
 				c++
 			}
 		}
-		grs = sortByName(grs, s, c)
+		grs = sortGroupsByName(grs, s, c)
 		parentID = grs[i].GroupID
 	}
 	return grs
@@ -145,13 +174,125 @@ func getTopParents(grs []group) []group {
 			topParents = append(topParents, grs[i])
 		}
 	}
-	grs = sortByName(grs, 0, len(grs))
+	grs = sortGroupsByName(grs, 0, len(grs))
 	return topParents
+}
+
+func groupsHandler(w http.ResponseWriter, r *http.Request) {
+	method := r.Method
+	switch method {
+	case "GET":
+	case "POST":
+	case "PUT":
+	case "DELETE":
+
+	}
+}
+
+func tasksListHandler(w http.ResponseWriter, r *http.Request) {
+	method := r.Method
+	if method != "GET" {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+	l := r.URL.Query().Get("limit")
+	s := r.URL.Query().Get("sort")
+	t := r.URL.Query().Get("type")
+	newTasks := getSortedTasks(tasks, s, l, t)
+	err := json.NewEncoder(w).Encode(newTasks)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func getSortedTasks(ts []task, s string, l string, t string) []task {
+	var newTasks []task
+	switch s {
+	case "name":
+		newTasks = sortTasksByName(ts)
+	case "group":
+		newTasks = sortTasksByGroup(ts)
+	default:
+		newTasks = ts
+	}
+	switch t {
+	case "completed":
+		newTasks = getCompletedTasks(ts)
+	case "working":
+		newTasks = getWorkingTasks(ts)
+	}
+	lim, err := strconv.Atoi(l)
+	if err != nil || lim < 0 {
+		return ts
+	}
+	if lim > len(ts) {
+		lim = len(ts)
+	}
+	newTasks = newTasks[:lim]
+	return newTasks
+}
+
+func sortTasksByName(ts []task) []task {
+	for i := 0; i < len(ts); i++ {
+		if ts[i].Task < ts[i-1].Task {
+			t := ts[i]
+			g := i
+			for g > 0 && t.Task < ts[g-1].Task {
+				ts[g] = ts[g-1]
+				g--
+			}
+			ts[g] = t
+		}
+	}
+	return ts
+}
+
+func sortTasksByGroup(ts []task) []task {
+	for i := 0; i < len(ts); i++ {
+		if ts[i].GroupID < ts[i-1].GroupID {
+			t := ts[i]
+			g := i
+			for g > 0 && t.GroupID < ts[g-1].GroupID {
+				ts[g] = ts[g-1]
+				g--
+			}
+			ts[g] = t
+		}
+	}
+	return ts
+}
+
+func getCompletedTasks(ts []task) []task {
+	for i := 0; i < len(ts); i++ {
+		if !ts[i].Completed {
+			ts = removeTask(ts, i)
+		}
+	}
+	return ts
+}
+
+func getWorkingTasks(ts []task) []task {
+	for i := 0; i < len(ts); i++ {
+		if ts[i].Completed {
+			ts = removeTask(ts, i)
+		}
+	}
+	return ts
+}
+
+func removeTask(ts []task, n int) []task {
+	for i := n; i < len(ts)-1; i++ {
+		ts[i] = ts[i+1]
+	}
+	ts = ts[:len(ts)-1]
+	return ts
 }
 
 func main() {
 	r := mux.NewRouter()
-	r.HandleFunc("/groups", groupsHandler)
-	http.Handle("/", r)
+	r.HandleFunc("/groups", groupsListHandler)
+	r.HandleFunc("/groups/", groupsHandler)
+	r.HandleFunc("/tasks", tasksListHandler)
+	r.Handle("/", r)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
